2213ef6614a836b3f7f2ef65481a7a7f382de8cb
Mohammed Ekholy <>
2024-02-06 04:59:43 +0100

    MariaDB second configuration

--- srcs/requirements/mariadb/tools/setting_up_mariadb.sh
+++ srcs/requirements/mariadb/tools/setting_up_mariadb.sh
@@ -3,22 +3,25 @@
 # Create the WordPress database on MariaDB server and configure it.
 create_configure_database()
 {
-	mysql -u$MARIADB_ROOT_USER -p$MARIADB_ROOT_PASS << EOF
-	CREATE DATABASE $MARIADB_NAME ;
+	mysql -u"$MARIADB_ROOT_USER" -p"$MARIADB_ROOT_PASS" << EOF
+	CREATE DATABASE IF NOT EXISTS $MARIADB_NAME ;
 	CREATE USER '$WP_USER'@'%' IDENTIFIED BY '$WP_USER_PASS' ;
 	GRANT ALL PRIVILEGES ON $MARIADB_NAME.* TO '$WP_USER'@'%' ;
 	FLUSH PRIVILEGES;
 EOF
-	return ($?)
+	return $?
 }
 
 # Start MariaDB server.
 service mariadb start
 create_configure_database
-If [ $? = 1 ]; then
+if [ $? = 1 ]; then
 	echo "MariaDB Configuration failed!"
 	exit
+else
+	echo "MariaDB has been configured successfully!"
 fi
 
-# Change the access for the WP_USER from localhost to outside hosts
-sed -i -E "s/127.0.0.1/0.0.0.0/1" /etc/mysql/mariadb.conf.d/50-server.cnf
+# Stop MariaDB service to make sure there is no other instances running
+service mariadb stop
+mysqld
