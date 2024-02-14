7b4f6517a2a37d643ad081e43d81f8a1bc6e5a62
SolarisCode <28461793+SolarisCode@users.noreply.github.com>
2024-02-14 01:35:48 +0100

    Update setting_up_nginx.sh

--- srcs/requirements/nginx/tools/setting_up_nginx.sh
+++ srcs/requirements/nginx/tools/setting_up_nginx.sh
@@ -3,14 +3,14 @@
 setting_up_nginx()
 {
        chown -hR 33:33 /var/www/melkholy.42.fr
-       ln -s /etc/nginx/sites-available/melkholy.42.fr /etc/nginx/sites-enabled/
        openssl req \
-              -newkey rsa:2048 -nodes -keyout /etc/ssl/private/melkholy.key \
-              -x509 -days 365 -out /etc/ssl/certs/melkholy.crt \
+              -newkey rsa:2048 -nodes -keyout $SSL_KEY \
+              -x509 -days 365 -out $SSL_CERT \
               -subj "/C=DE/ST=Lower Saxon/L=Wolfsburg/O=Melkholy 42Wolfsburg/CN=melkholy.42.com"
+       envsubst < melkholy.42.fr > /etc/nginx/sites-available/default
        # openssl req \
-       #        -newkey rsa:2048 -nodes -keyout /etc/ssl/private/melkholy.switzerlandnorth.cloudapp.azure.com.key \
-       #        -x509 -days 365 -out /etc/ssl/certs/melkholy.switzerlandnorth.cloudapp.azure.com.crt \
+       #        -newkey rsa:2048 -nodes -keyout $AZURE_KEY \
+       #        -x509 -days 365 -out $AZURE_CERT \
        #        -subj "/C=DE/ST=Lower Saxon/L=Wolfsburg/O=Melkholy 42Wolfsburg/CN=melkholy.switzerlandnorth.cloudapp.azure.com"
 }
 
